f02a16e38f0e0f2972141fe4375905ae1e1f9f7d
Crafset <82468619+Crafset@users.noreply.github.com>
2021-08-22 11:53:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+import time
+
+print
+print("___________________    _____  __________________________________________")
+print("\_   ___ \______   \  /  _  \ \_   _____/   _____/\_   _____/\__    ___/")
+print("/    \  \/|       _/ /  /_\  \ |    __) \_____  \  |    __)_   |    ")
+print("\     \___|    |   \/    |    \|     \  /        \ |        \  |    |")
+print(" \______  /____|_  /\____|__  /\___  / /_______  //_______  /  |____|")
+print("        \/       \/         \/     \/          \/         \/")
+print ("Crafset  : AZERT-HACK")
+print ("Team : CYBER ATTACK ")
+print ("Discord : https://discord.gg/GcQAWW4kUZ")
+print
+print("______________________________________")
+
+print ("[+]-->                 [+] 0%")
+time.sleep(2)
+print ("[+]------>                 [+] 25%")
+time.sleep(2)
+print ("[+]---------->                 [+] 50%")
+time.sleep(3)
+print ("[+]------------------>              [+] 75%")
+time.sleep(2)
+print ("[+]--------------------->               [+] 100%")
+
+os.system("nmap -sn 192.168.1.1/24")
+
+fin = input(".")
+
+os.system("cls")
+
+
+
+
+
+
+
+
+
+
